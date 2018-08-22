b12d7c5603214c4a93271a04748f272004d65b61
xa-xa-xa <askrasn@gmail.com>
2018-08-22 15:02:21 -0700

    1.0.1

--- contactmanager/src/Components/Contacts.js
+++ contactmanager/src/Components/Contacts.js
@@ -1,45 +1,11 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
+import { Consumer } from "../context";
 
 class Contacts extends Component {
   // constructor() {  // ---this is optional if we need itialisation
   //   super();       //
   //   this.state = { //
-  state = {
-    contacts: [
-      {
-        id: "1",
-        name: "Jond Doe",
-        email: "jdoe@email.com",
-        phone: "111-111-1111"
-      },
-      {
-        id: "2",
-        name: "Peter Smith",
-        email: "psmith@email.com",
-        phone: "222-222-2222"
-      },
-      {
-        id: "3",
-        name: "Marta Johnes",
-        email: "mjohnes@email.com",
-        phone: "333-333-3333"
-      },
-      {
-        id: "4",
-        name: "Will Marks",
-        email: "wmarks@email.com",
-        phone: "444-444-4444"
-      },
-      {
-        id: "5",
-        name: "Glen Britch",
-        email: "gbritch@email.com",
-        phone: "555-555-5555"
-      }
-    ]
-    // }  //
-  };
 
   deleteContact = id => {
     const { contacts } = this.state;
--- contactmanager/src/context.js
+++ contactmanager/src/context.js
@@ -1,3 +1,49 @@
 import React, { Component } from "react";
 
 const Context = React.createContext();
+
+export class Provider extends Component {
+  state = {
+    contacts: [
+      {
+        id: "1",
+        name: "Jond Doe",
+        email: "jdoe@email.com",
+        phone: "111-111-1111"
+      },
+      {
+        id: "2",
+        name: "Peter Smith",
+        email: "psmith@email.com",
+        phone: "222-222-2222"
+      },
+      {
+        id: "3",
+        name: "Marta Johnes",
+        email: "mjohnes@email.com",
+        phone: "333-333-3333"
+      },
+      {
+        id: "4",
+        name: "Will Marks",
+        email: "wmarks@email.com",
+        phone: "444-444-4444"
+      },
+      {
+        id: "5",
+        name: "Glen Britch",
+        email: "gbritch@email.com",
+        phone: "555-555-5555"
+      }
+    ]
+  };
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export const Consumer = Context.Consumer;
